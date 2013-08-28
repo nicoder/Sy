74c94930ddb2ae5bcd60430bbaf8ed7dd7471366
Baptiste Langlade <baptouuuu@gmail.com>
2013-08-28 20:23:01 +0200

    modified the pseudo app

--- app/data.js
+++ app/data.js
@@ -3,18 +3,10 @@ namespace('App');
 var stackFact = Sy.service.get('core::stack::factory'),
     storeFact = Sy.service.get('core::storage::factory'),
     myStack = stackFact.get('todo'),
-    standaloneStorage = storeFact.get('localstorage', 'standalone'),
     e = new App.Entity.Todo({
         name: 'foo'
     });
 
 myStack.setStorage(storeFact.get('localstorage'));
 
-myStack.persist(e);
-
-myStack.flush();
-
-// standaloneStorage.create({
-//     prop: 'value'
-// });
-// standaloneStorage.flush();
+myStack.retrieve();
